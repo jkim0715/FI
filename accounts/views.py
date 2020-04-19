c0c0730871ea862322341388fc34df52920ee774
jkim0715 <gjr191@naver.com>
2020-04-19 17:27:52 +0900

    Accounts | Make Signup/Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import get_user_model #회원가입 유저/
-from django.contrib.auth.forms import AuthenticationForm #로그인 form
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm #로그인 form
 from django.contrib.auth import login as auth_login #로그인 정보 세션에 저장
+
 User = get_user_model() # 유저 Import
 
 # Create your views here.
 def index(request):
+    accounts = User.objects.all()
+    context ={
+       'accounts': accounts 
+    }
+    return render(request, 'accounts/index.html',context)
 
-    return render(request, 'accounts/index.html')
+def signup(request):
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+
+    form = UserCreationForm()
+    context={
+        'form':form
+    }
+    return render(request, 'accounts/form.html',context)
 
 
 def login(request):
@@ -18,11 +35,10 @@ def login(request):
                 return redirect(request.GET.get('next') or 'accounts:index')
     else:
         form = AuthenticationForm()
-
     context ={
         'form' : form
     }
-    return ''
+    return render(request,'accounts/form.html', context)
 
 def logout(request):
     pass
