d2a8e8d01bd1a69cf3595dfc8893a2a88370b211
jkim0715 <gjr191@naver.com>
2020-04-25 15:08:23 +0900

    Posts | Add Posts

---
+++ posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+# from django.contrib.auth.model import User  // 이거 사용하지 않는 이유 ?
+
+
+# Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now =True)
+    
+
+class Comment(models.Model):
+    content = models.models.CharField( max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now =True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
