2d401fef082bd9cebeb783598fde402fedaeb0a6
jkim0715 <gjr191@naver.com>
2020-04-26 22:32:46 +0900

    POSTS | Add posts CRUD

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name="posts"
+urlpatterns = [
+    path('create/', views.create, name="create"),
+    path('update/<int:post_id>/', views.update, name="update"),
+    path('delete/<int:post_id>/', views.delete, name="delete"),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create(request):
+    return ''
+
+def update(request,post_id):
+
+    return ''
+
+
+def delete(request,post_id):
+
+    return
